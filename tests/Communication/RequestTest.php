1df13b7214883e305953ec004215948ed0e74dae
Xawier Slupik <48760619+xBlixon@users.noreply.github.com>
2024-02-02 20:49:52 +0100

    added request method test

--- tests/Communication/RequestTest.php
+++ tests/Communication/RequestTest.php
@@ -4,11 +4,23 @@ namespace Communication;
 
 
 use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\Attributes\Test;
+use PHPUnit\Framework\Attributes\TestDox;
 use PHPUnit\Framework\TestCase;
 use Velsym\Communication\Request;
 
 #[CoversClass(Request::class)]
 final class RequestTest extends TestCase
 {
-
+    /**
+     * Tests if Request class returns correct HTTP method.
+     */
+    #[Test]
+    #[TestDox("Returns correct HTTP method")]
+    public function method(): void
+    {
+        $method = "POST";
+        $_SERVER['REQUEST_METHOD'] = $method;
+        $this->assertSame($method, Request::method());
+    }
 }
