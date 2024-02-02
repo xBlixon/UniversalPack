a19e5fc03dee7bc4aeff1bc27b1b7e791d1908e6
Xawier Slupik <48760619+xBlixon@users.noreply.github.com>
2024-02-02 20:58:31 +0100

    added test for request url

--- tests/Communication/RequestTest.php
+++ tests/Communication/RequestTest.php
@@ -8,6 +8,8 @@ use PHPUnit\Framework\Attributes\Test;
 use PHPUnit\Framework\Attributes\TestDox;
 use PHPUnit\Framework\TestCase;
 use Velsym\Communication\Request;
+use Velsym\Communication\UrlInfo;
+
 
 #[CoversClass(Request::class)]
 final class RequestTest extends TestCase
@@ -22,5 +24,19 @@ final class RequestTest extends TestCase
         $method = "POST";
         $_SERVER['REQUEST_METHOD'] = $method;
         $this->assertSame($method, Request::method());
+    }
+
+    /**
+     * Tests if Request class returns UrlInfo object
+     * that reflects actual HTTP request.
+     */
+    #[Test]
+    #[TestDox("Returns correct UrlInfo object")]
+    public function url(): void
+    {
+        $uri = "https://example.com/a/b/c#test?foo=bar&baz=foo";
+        $_SERVER['REQUEST_URI'] = $uri;
+        $urlInfo = new UrlInfo($uri);
+        $this->assertEquals($urlInfo, Request::url());
     }
 }
