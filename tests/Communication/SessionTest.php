5b7c15aafc11aa712d001fb6896947455cbaa691
Xawier Slupik <48760619+xBlixon@users.noreply.github.com>
2024-02-07 16:06:35 +0100

    added session setting-getting test

--- tests/Communication/SessionTest.php
+++ tests/Communication/SessionTest.php
@@ -3,11 +3,29 @@
 namespace Communication;
 
 use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\Attributes\Test;
+use PHPUnit\Framework\Attributes\TestDox;
 use PHPUnit\Framework\TestCase;
 use Velsym\Communication\Session;
 
 #[CoversClass(Session::class)]
 final class SessionTest extends TestCase
 {
+    private Session $session;
 
+    protected function setUp(): void
+    {
+        $this->session = Session::getInstance();
+    }
+
+    /**
+     * This test check setting-getting functionality.
+     */
+    #[Test]
+    #[TestDox("Setting-Getting")]
+    public function settingGetting()
+    {
+        $this->session->set('foo', 'bar');
+        $this->assertSame('bar', $this->session->get('foo'));
+    }
 }
